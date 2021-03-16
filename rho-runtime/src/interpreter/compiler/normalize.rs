2ecd00dfcebcfa35d039a2f14ea2ccf1425e8f55
jerry <vcer@qq.com>
2021-03-16 16:32:26 +0800

    + prev commit

--- rho-runtime/src/interpreter/compiler/normalize.rs
+++ rho-runtime/src/interpreter/compiler/normalize.rs
@@ -1,5 +1,22 @@
-use super::context::{ SourcePosition, ProcVisitInputs, ProcVisitOutputs };
+use super::context::{ SourcePosition, IndexMapChain, DeBruijnLevelMap };
 use super::bnfc;
+
+use super::super::super::model::rho_types::*;
+
+
+// Input data to the normalizer
+pub struct ProcVisitInputs {
+    pub par : Par,
+    pub env : IndexMapChain,
+    pub known_free : DeBruijnLevelMap,
+}
+
+pub struct ProcVisitOutputs {
+    pub par : Par,
+    pub known_free : DeBruijnLevelMap,
+}
+
+
 
 pub struct NormalizeResult {
     // warning messages
