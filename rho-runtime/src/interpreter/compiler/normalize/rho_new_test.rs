5f799ed182d1ddbbaaf57a15fdd2aafaafae604c
jerry <vcer@qq.com>
2021-03-20 17:15:11 +0800

    added normalizer of par

---
+++ rho-runtime/src/interpreter/compiler/normalize/rho_new_test.rs
@@ -0,0 +1,16 @@
+
+
+use std::cmp::Ordering;
+use std::collections::hash_set::HashSet;
+
+use super::*;
+use super::super::bnfc;
+
+use super::super::errors::*;
+use super::{ ProcVisitInputs, ProcVisitOutputs};
+
+
+#[test]
+fn pnew_should_bind_new_variables() {
+    
+}
