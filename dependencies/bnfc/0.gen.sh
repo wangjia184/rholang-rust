3353fd370868637b2c073c06d5a460ab8483b91e
jerry <vcer@qq.com>
2021-03-25 11:09:15 +0800

    split test cases to a seperated project

--- dependencies/bnfc/0.gen.sh
+++ dependencies/bnfc/0.gen.sh
@@ -21,7 +21,7 @@ ar rcs ../libbnfc.a Absyn.o Buffer.o Lexer.o Parser.o Printer.o
 cd ..
 
 # be careful with the platform
-mv -f ./libbnfc.a ../../rholang-parser/lib/x86_64-apple-darwin/
-#mv -f ./libbnfc.a ../../rholang-parser/lib/aarch64-apple-darwin/
-#mv -f ./libbnfc.a ../../rholang-parser/lib/x86_64-unknown-linux-gnu/
+#mv -f ./libbnfc.a ../../rholang_parser/lib/x86_64-apple-darwin/
+#mv -f ./libbnfc.a ../../rholang_parser/lib/aarch64-apple-darwin/
+#mv -f ./libbnfc.a ../../rholang_parser/lib/x86_64-unknown-linux-gnu/
 
