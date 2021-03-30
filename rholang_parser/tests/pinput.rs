a0b5b96fb83a42b2e8fa2fe48180cb87bd06df86
jerry <vcer@qq.com>
2021-03-31 07:18:06 +0800

    added test case pinput_should_handle_peek()

--- rholang_parser/tests/pinput.rs
+++ rholang_parser/tests/pinput.rs
@@ -184,4 +184,14 @@ fn pinput_should_handle_a_simple_receive() {
 }
 
 
+#[test]
+fn pinput_should_handle_peek() {
+    let rholang_code = "for ( x, y <<- @Nil ) { x!(*y) }";
 
+    let mut result = common::run_normalizer(rholang_code);
+    let root_par = result.par.take().unwrap();
+
+    assert_eq!( root_par.receives.len(), 1);
+    let rho_receive = &root_par.receives[0];
+    assert_eq!( rho_receive.peek, true);
+}
