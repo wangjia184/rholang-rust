b360d444cdf0fd6e5fee8598602ff66e951a74ae
jerry <vcer@qq.com>
2021-04-14 10:34:37 +0800

    test case news_should_be_sorted_basing_on_bind_count_urls_and_then_body()

--- model/src/ordering/sort_new_test.rs
+++ model/src/ordering/sort_new_test.rs
@@ -1,32 +1,13 @@
-use super::*;
 
-#[cfg(test)] 
-fn traverse(depth : usize, iter : ScoreTreeIter) {
-    for node in iter {
-        match node {
-            Node::Leaf(scored_atom) => {
-                match scored_atom {
-                    ScoreAtom::IntAtom(i) => println!("{:indent$}ScoreAtom::IntAtom({})", "", i, indent=depth*2),
-                    ScoreAtom::StringAtom(s) => println!("{:indent$}ScoreAtom::StringAtom({})", "", s, indent=depth*2),
-                    _ => { panic!("Unknown type") },
-                }
-                
-            },
-            Node::Children(iter) => {
-                println!("{:indent$}{{", "", indent=depth*2);
-                traverse(depth+1, iter);
-                println!("{:indent$}}}", "", indent=depth*2);
-            }
-        }
-        
-    }
-}
+
 
 
 
 
 #[test]
 fn news_should_be_sorted_basing_on_bind_count_urls_and_then_body() {
+    use super::*;
+
     let mut par = Par::default();
     {
         let mut rho_new = New::default();
@@ -68,70 +49,95 @@ fn news_should_be_sorted_basing_on_bind_count_urls_and_then_body() {
         par.news.push(rho_new);
     }
 
-    // Node(List(
-    //     Leaf(ScoreAtom(IntAtom(999))),
-    //     Node(List(
-    //         Leaf(ScoreAtom(IntAtom(303))),
-    //         Leaf(ScoreAtom(IntAtom(1))),
-    //         Leaf(ScoreAtom(IntAtom(0))),
-    //         Leaf(ScoreAtom(IntAtom(0))),
-    //         Node(List(
-    //             Leaf(ScoreAtom(IntAtom(999))),
-    //             Leaf(ScoreAtom(IntAtom(0)))
-    //         ))
-    //     )),
-    //     Node(List(
-    //         Leaf(ScoreAtom(IntAtom(303))),
-    //         Leaf(ScoreAtom(IntAtom(2))),
-    //         Leaf(ScoreAtom(IntAtom(0))),
-    //         Leaf(ScoreAtom(IntAtom(0))),
-    //         Node(List(
-    //             Leaf(ScoreAtom(IntAtom(999))),
-    //             Leaf(ScoreAtom(IntAtom(0)))
-    //         ))
-    //     )),
-    //     Node(Vector(
-    //         Leaf(ScoreAtom(IntAtom(303))),
-    //         Leaf(ScoreAtom(IntAtom(2))),
-    //         Leaf(ScoreAtom(StringAtom(rho:io:stderr))),
-    //         Leaf(ScoreAtom(IntAtom(0))),
-    //         Node(List(
-    //             Leaf(ScoreAtom(IntAtom(999))),
-    //             Leaf(ScoreAtom(IntAtom(0)))
-    //         ))
-    //     )), 
-    //     Node(Vector(
-    //         Leaf(ScoreAtom(IntAtom(303))),
-    //         Leaf(ScoreAtom(IntAtom(2))),
-    //         Leaf(ScoreAtom(StringAtom(rho:io:stdout))),
-    //         Leaf(ScoreAtom(IntAtom(0))), 
-    //         Node(List(
-    //             Leaf(ScoreAtom(IntAtom(999))),
-    //             Leaf(ScoreAtom(IntAtom(0)))
-    //         ))
-    //     )),
-    //     Node(Vector(
-    //         Leaf(ScoreAtom(IntAtom(303))),
-    //         Leaf(ScoreAtom(IntAtom(2))),
-    //         Leaf(ScoreAtom(StringAtom(rho:io:stdout))),
-    //         Leaf(ScoreAtom(IntAtom(0))),
-    //         Node(List(
-    //             Leaf(ScoreAtom(IntAtom(999))),
-    //             Node(ArrayBuffer(
-    //                 Leaf(ScoreAtom(IntAtom(2))),
-    //                 Leaf(ScoreAtom(IntAtom(7)))
-    //             )), 
-    //             Leaf(ScoreAtom(IntAtom(0)))
-    //         ))
-    //     )), 
-    //     Leaf(ScoreAtom(IntAtom(0)))
-    // ))
+
     par.sort();
-    traverse(0, par.score_tree_iter());
+    let mut output = String::new();
+    traverse(par.score_tree_iter(), 0, &mut output);
+
+    let target = "ScoreAtom::IntAtom(999)
+{
+  ScoreAtom::IntAtom(303)
+  ScoreAtom::IntAtom(1)
+  ScoreAtom::IntAtom(0)
+  ScoreAtom::IntAtom(0)
+  {
+    ScoreAtom::IntAtom(999)
+    ScoreAtom::IntAtom(0)
+  }
+}
+{
+  ScoreAtom::IntAtom(303)
+  ScoreAtom::IntAtom(2)
+  ScoreAtom::IntAtom(0)
+  ScoreAtom::IntAtom(0)
+  {
+    ScoreAtom::IntAtom(999)
+    ScoreAtom::IntAtom(0)
+  }
+}
+{
+  ScoreAtom::IntAtom(303)
+  ScoreAtom::IntAtom(2)
+  ScoreAtom::StringAtom(rho:io:stderr)
+  ScoreAtom::IntAtom(0)
+  {
+    ScoreAtom::IntAtom(999)
+    ScoreAtom::IntAtom(0)
+  }
+}
+{
+  ScoreAtom::IntAtom(303)
+  ScoreAtom::IntAtom(2)
+  ScoreAtom::StringAtom(rho:io:stdout)
+  ScoreAtom::IntAtom(0)
+  {
+    ScoreAtom::IntAtom(999)
+    ScoreAtom::IntAtom(0)
+  }
+}
+{
+  ScoreAtom::IntAtom(303)
+  ScoreAtom::IntAtom(2)
+  ScoreAtom::StringAtom(rho:io:stdout)
+  ScoreAtom::IntAtom(0)
+  {
+    ScoreAtom::IntAtom(999)
+    {
+      ScoreAtom::IntAtom(2)
+      ScoreAtom::IntAtom(7)
+    }
+    ScoreAtom::IntAtom(0)
+  }
+}
+ScoreAtom::IntAtom(0)
+";
     
-    //println!("{:#?}", &par);
-
-
+    
+        fn traverse(iter : ScoreTreeIter<'_>, depth : usize, output : &mut String ) {
+            for node in iter {
+                match node {
+                    Node::Leaf(scored_atom) => {
+                        match scored_atom {
+                            ScoreAtom::IntAtom(i) =>
+                                output.push_str(&format!("{:indent$}ScoreAtom::IntAtom({})\n", "", i, indent=depth*2) ),
+                            ScoreAtom::StringAtom(s) =>
+                                output.push_str(&format!("{:indent$}ScoreAtom::StringAtom({})\n", "", s, indent=depth*2)),
+                            _ => { panic!("Unknown type") },
+                        }
+                        
+                    },
+                    Node::Children(iter) => {
+                        output.push_str(&format!("{:indent$}{{\n", "", indent=depth*2));
+                        traverse(iter, depth+1, output);
+                        output.push_str(&format!("{:indent$}}}\n", "", indent=depth*2));
+                    }
+                }
+                
+            }
+        }
+    
+        println!("{}", &output);
+        assert!(output.eq(target));
 
     
 }
