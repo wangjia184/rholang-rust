6fde6c8d4e555639c561e77483b4581d3cb92e01
jerry <vcer@qq.com>
2021-04-28 19:56:48 +0800

    + prev commit

--- docker/build.sh
+++ docker/build.sh
@@ -2,8 +2,11 @@
 
 #CARGO_PROFILE_RELEASE_LTO=true cargo build --release
 
-mkdir ./app
+mkdir -p ./app
 cp ../target/release/rho_runtime -f ./app/rho_runtime
 cp ../target/release/rholang_parser -f ./app/rholang_parser
 
-sudo docker build .
+IMAGE_URI=wangjia184/rho_runtime:0.0.1
+
+sudo docker build -t=$IMAGE_URI .
+sudo docker push  $IMAGE_URI
